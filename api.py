ae3223694752a16639131305b5ef0e01b122d1e3
levch <levchenko.d.a1998@gmail.com>
2022-06-02 23:23:22 +0300

    .

--- api.py
+++ api.py
@@ -1,12 +1,16 @@
 class Tags:
     _a = '<a href="{url}">{text}</a>'
     _pre = '<pre>{}</pre>'
+    _textarea = '<textarea cols="{cols}" rows="{rows}">{}</textarea>'
 
     def a(self, text: str, url: str):
         return self._a.format(text=text, url=url)
 
     def pre(self, text: str):
         return self._pre.format(text)
+
+    def textarea(self, text: str = '', cols: str = '', rows: str = '', ):
+        return self._textarea.format(text, cols=cols, rows=rows)
 
 
 tags = Tags()
--- views.py
+++ views.py
@@ -14,12 +14,17 @@ def index():
 
 
 @app.route('/bot/')
-def bot_root():
-    return bot_path(req_path='')
+def bot():
+    return bot_file(req_path='')
+
+
+@app.route('/test')
+def test():
+    return tags.textarea('Bebra')
 
 
 @app.route('/bot/<path:req_path>')
-def bot_path(req_path: str):
+def bot_file(req_path: str):
     file_path = f'{BOT_PATH}/{req_path}'
 
     if not os.path.exists(file_path):
@@ -32,10 +37,10 @@ def bot_path(req_path: str):
 
         for i in items[1] + items[2]:
             i_path = f'{req_path}/{i}'.lstrip('/')
-            i_url = tags.a(i, url_for('bot_path', req_path=i_path))
+            i_url = tags.a(i, url_for('bot_file', req_path=i_path))
             strings.append(i_url)
 
         return tags.pre('\n'.join(strings))
 
     with open(file_path) as file:
-        return tags.pre(file.read())
+        return tags.textarea(file.read(), cols=100, rows=30)
